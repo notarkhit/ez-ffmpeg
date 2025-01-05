de52cce2f09c1405b5edd544194f928db15be98f
notarkhit <notarkhit@gmail.com>
2025-01-05 18:10:36 +0530

    remove space

--- ez-ffmpeg.sh
+++ ez-ffmpeg.sh
@@ -14,7 +14,7 @@ else
     echo "ERROR: No arguements ! "
 fi
 
-# TODO: 
+# TODO:
 # add the operation for ffmpeg
 #
 # ffmpeg -i input.mp4 -c:v libwebp -vf "fps=10,scale=1920:-1" -lossless 0 -q:v 80 output.webp
