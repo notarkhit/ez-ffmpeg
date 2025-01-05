d7ea3cae5c1bf9f1055b242732350555b672d50b
notarkhit <notarkhit@gmail.com>
2025-01-05 11:57:42 +0530

    close if

--- ez-ffmpeg.sh
+++ ez-ffmpeg.sh
@@ -3,7 +3,7 @@
 if ! [ -z "$1" ]; then
     filename=$(echo "$1" | awk -F '.' '{print $1}')
     extension=$(echo "$1" | awk -F '.' '{print $2}')
-if
+fi
 
 if  [ -z "$extension" ]; then
     echo "No"
