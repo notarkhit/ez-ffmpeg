3424b349e5a5d62cc7530dceb18ade091407edc6
notarkhit <notarkhit@gmail.com>
2025-01-05 14:12:00 +0530

    test args

--- ez-ffmpeg.sh
+++ ez-ffmpeg.sh
@@ -5,14 +5,12 @@ if ! [ -z "$1" ]; then
     extension=$(echo "$1" | awk -F '.' '{print $2}')
 fi
 
-if  [ -z "$extension" ]; then
-    echo "ERROR: No file extension"
-    exit 1
-fi
-
-else
-    echo "ERROR: No arguements ! "
-fi
-
-echo "completed"
-
+# if  [ -z "$extension" ]; then
+#     echo "ERROR: No file extension"
+#     exit 1
+# else
+#     echo "ERROR: No arguements ! "
+# fi
+#
+# echo "completed"
+#
