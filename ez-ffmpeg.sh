1bd8f795e73cd492c33dcff8534f17097021c5d8
notarkhit <notarkhit@gmail.com>
2025-01-05 14:28:14 +0530

    add todo

--- ez-ffmpeg.sh
+++ ez-ffmpeg.sh
@@ -3,14 +3,22 @@
 if ! [ -z "$1" ]; then
     filename=$(echo "$1" | awk -F '.' '{print $1}')
     extension=$(echo "$1" | awk -F '.' '{print $2}')
+	outputFile=$filename.webp
+	echo $outputFilename
 fi
 
-# if  [ -z "$extension" ]; then
-#     echo "ERROR: No file extension"
-#     exit 1
-# else
-#     echo "ERROR: No arguements ! "
-# fi
+if  [ -z "$extension" ]; then
+    echo "ERROR: No file extension"
+    exit 1
+else
+    echo "ERROR: No arguements ! "
+fi
+
+# TODO: 
+# add the operation for ffmpeg
 #
-# echo "completed"
+# ffmpeg -i input.mp4 -c:v libwebp -vf "fps=10,scale=1920:-1" -lossless 0 -q:v 80 output.webp
 #
+
+echo "completed"
+
