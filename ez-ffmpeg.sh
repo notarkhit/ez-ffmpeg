5f8cdb4a15a8c7bb7dc56b1f23e3b4b4bf96932c
Crimson-Genesis <nico.zero.0x@gmail.com>
2025-01-05 11:53:24 +0530

    update in the ez-ffmpeg.sh

--- ez-ffmpeg.sh
+++ ez-ffmpeg.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-if ! [ -z "$1" ]; then 
-	filename=$(echo "$1" | awk -F '.' '{print $1}')
-	extension=$(echo "$1" | awk -F '.' '{print $2}')
+if ! [ -z "$1" ]; then
+    filename=$(echo "$1" | awk -F '.' '{print $1}')
+    extension=$(echo "$1" | awk -F '.' '{print $2}')
+if
 
-	if  [ -z "$extension" ]; then
-		echo "No"
-		exit 1
-	fi
+if  [ -z "$extension" ]; then
+    echo "No"
+    exit 1
+fi
 
 else
-	echo "ERROR: No arguements ! "
+    echo "ERROR: No arguements ! "
 fi
 
 echo "completed"
