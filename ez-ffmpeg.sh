bba782b4d8c4374d9ba84ffa6215052bc5e6c74d
Crimson-Genesis <nico.zero.0x@gmail.com>
2025-01-07 01:28:13 +0530

    changed the ez-ffmpeg.sh script.

--- ez-ffmpeg.sh
+++ ez-ffmpeg.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-if ! [ -z "$1" ]; then
-    filename=$(echo "$1" | awk -F '.' '{print $1}')
-    extension=$(echo "$1" | awk -F '.' '{print $2}')
-    # Hello, World
-fi
+function ffmpeg_function(){
+    ffmpeg -i $filename.$extension -c:v libwebp -vf "fps=10,scale=1920:-1" -lossless 0 -q:v 80 $outputFile
+}
 
-if  [ -z "$extension" ]; then
-    echo "No"
-    exit 1
-fi
+function main (){
+    if [[ -e "$1" ]]; then
+        base_filename=$(basename $1)
+        filename=${base_filename%.*}
+        extension=${base_filename##*.}
+        outputFile=$filename.webp
 
-outputFile=$filename.webp
-echo $outputFilename
-ffmpeg -i $filename.$extension -c:v libwebp -vf "fps=10,scale=1920:-1" -lossless 0 -q:v 80 $outputFile
-else
-    echo "ERROR: No arguements ! "
-fi
+        if ! cat .formats | awk 'NR >= 5 {print $2}' | grep -qx "$extension" ; then
+            echo -e "\033[38;5;1mFILE FORMAT NOT SUPPORTED.\033[0m"
+            exit 1
+        fi
 
-if  [ -z "$extension" ]; then
-    echo "ERROR: No file extension"
-    exit 1
-fi
+        # Calling the ffmpeg function here -
+        ffmpeg_function
 
-echo  "process completed, file saved at :"
-pwd
+        echo -e "\033[38;5;2mSaved the output file at: $(pwd)/$outputFile\033[0m"
+    else
+        echo -e "\033[38;5;1mIncorrect file path !!!\033[0m"
+    fi
+}
+
+main $1
+
+# Have fun now 😉.
