357089286218201260e78dbee502589186b73666
notarkhit <notarkhit@gmail.com>
2025-01-06 08:58:39 +0530

    fix error condition

--- ez-ffmpeg.sh
+++ ez-ffmpeg.sh
@@ -6,13 +6,13 @@ if ! [ -z "$1" ]; then
 	outputFile=$filename.webp
 	echo $outputFilename
 	ffmpeg -i $filename.$extension -c:v libwebp -vf "fps=10,scale=1920:-1" -lossless 0 -q:v 80 $outputFile
+else
+    echo "ERROR: No arguements ! "
 fi
 
 if  [ -z "$extension" ]; then
     echo "ERROR: No file extension"
     exit 1
-else
-    echo "ERROR: No arguements ! "
 fi
 
 echo "completed"
